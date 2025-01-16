1a659c372fdd3df7fe73c0719b8a2b42e0d99861
syb0rg <ashworth.tylr@gmail.com>
2025-01-16 16:03:55 -0600

    Build: Improve macOS compatibility and CMake hygiene
    - Add PRIVATE visibility to all target_link_libraries calls
    - Fix iconv linking on macOS
    - Improve LLVM path detection using brew --prefix

--- autobuild/clang16_mac.cmake
+++ autobuild/clang16_mac.cmake
@@ -1,10 +1,16 @@
 cmake_minimum_required(VERSION 3.20)
-set(CMAKE_C_COMPILER "/usr/local/opt/llvm/bin/clang")
-set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm/bin/clang++")
 
+# Find LLVM installation
+execute_process(
+    COMMAND brew --prefix llvm
+    OUTPUT_VARIABLE LLVM_PREFIX
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_CXX_COMPILER "clang++")
 set(CMAKE_C_FLAGS_INIT "-fexperimental-library -Wno-parentheses")
 set(CMAKE_CXX_FLAGS_INIT "-nostdlib++ ${CMAKE_C_FLAGS_INIT}")
 set(CMAKE_OBJCXX_FLAGS_INIT "-nostdlib++ ${CMAKE_CXX_FLAGS_INIT}")
-
-set(CMAKE_EXE_LINKER_FLAGS_INIT "/usr/local/opt/llvm/lib/c++/libc++.a /usr/local/opt/llvm/lib/c++/libc++abi.a /usr/local/opt/llvm/lib/c++/libc++experimental.a")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "/usr/local/opt/llvm/lib/c++/libc++.a /usr/local/opt/llvm/lib/c++/libc++abi.a /usr/local/opt/llvm/lib/c++/libc++experimental.a")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${LLVM_PREFIX}/lib/c++/libc++.a ${LLVM_PREFIX}/lib/c++/libc++abi.a ${LLVM_PREFIX}/lib/c++/libc++experimental.a")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "${LLVM_PREFIX}/lib/c++/libc++.a ${LLVM_PREFIX}/lib/c++/libc++abi.a ${LLVM_PREFIX}/lib/c++/libc++experimental.a")
